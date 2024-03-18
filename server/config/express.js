5dc35274b724049c20d83d4029b1a7f26a3209e1
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-03-18 15:10:56 +0000

    update some dependencies

--- server/config/express.js
+++ server/config/express.js
@@ -84,7 +84,7 @@
     // Setup templating engine
     app.set('view engine', 'njk');
     app.set('views', [
-      'node_modules/govuk-frontend/',
+      'node_modules/govuk-frontend/dist',
       'node_modules/@ministryofjustice/frontend/',
       path.join(app.get('appPath'), 'templates'),
     ]);
