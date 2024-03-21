c2d5acf3e165c2e8cbfb6ac3c31be043e84663fc
benedwards <ben.edwards@cgi.com>
2024-03-21 17:24:58 +0000

    Added Smart Card spend upper limit

--- server/config/validation/add-smartcard-spend.js
+++ server/config/validation/add-smartcard-spend.js
@@ -18,6 +18,11 @@
             summary: 'Smartcard spend can only be 0 or more',
             details: 'Smartcard spend can only be 0 or more',
           },
+          lessThanOrEqualTo: 1000000,
+          notLessThanOrEqualTo: {
+            summary: 'Smartcard spend can not exceed 1,000,000',
+            details: 'Smartcard spend can not exceed 1,000,000',
+          },
           notValid: {
             summary: 'Smartcard spend can only include numbers and a decimal point',
             details: 'Smartcard spend can only include numbers and a decimal point',
