2115443719968eca31e6d439c8db48cdd4d9c756
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-03-25 12:05:15 +0000

    add simple validation for the initial 'search by' for export contact details

--- server/routes/messaging/export-contacts-controller.js
+++ server/routes/messaging/export-contacts-controller.js
@@ -2,17 +2,29 @@
   'use strict';
 
   const _ = require('lodash');
+  const validate = require('validate.js');
+  const exportContactDetailsValidator = require('../../config/validation/export-contact-details');
+  const { fetchAllCourts } = require('../../objects/request-pool');
+  const { transformCourtNames } = require('../../lib/mod-utils');
 
   module.exports.getExportContacts = function(app) {
-    return function(req, res) {
-      delete req.session.messaging;
-
+    return async function(req, res) {
+      let courtsResponse;
       const tmpErrors = _.clone(req.session.errors);
 
+      delete req.session.messaging;
       delete req.session.errors;
+
+      try {
+        courtsResponse = await fetchAllCourts.get(require('request-promise'), app, req.session.authToken);
+      } catch (err) {
+        // TODO: Handle error
+        console.log(err);
+      }
 
       return res.render('messaging/export-contact-details.njk', {
         nav: 'export',
+        courts: transformCourtNames(courtsResponse.courts),
         errors: {
           title: 'Please check the form',
           count: typeof tmpErrors !== 'undefined' ? Object.keys(tmpErrors).length : 0,
@@ -24,15 +36,10 @@
 
   module.exports.postExportContacts = function(app) {
     return function(req, res) {
-      const { searchBy } = req.body;
+      const validatorResult = validate(req.body, exportContactDetailsValidator(req.body));
 
-      if (!searchBy || searchBy === '') {
-        req.session.errors = {
-          searchBy: [{
-            details: 'Select how you want to search for contact details',
-            summary: 'Select how you want to search for contact details',
-          }],
-        };
+      if (validatorResult) {
+        req.session.errors = validatorResult;
 
         return res.redirect(app.namedRoutes.build('messaging.export-contacts.get'));
       }
