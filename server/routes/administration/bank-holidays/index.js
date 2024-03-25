3e6a5159f3b56c7671742beb80b6a503491bb561
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2024-03-25 08:17:33 +0000

    feature/JM-6246: View bank holidays (#96)
    * Implemented Administration Bank Holidays screen
    
    * fix align style
    
    ---------
    
    Co-authored-by: Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>

---
+++ server/routes/administration/bank-holidays/index.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  const auth = require('../../../components/auth');
+  const controller = require('./bank-holidays.controller');
+  const { isSystemAdministrator } = require('../../../components/auth/user-type');
+
+  module.exports = function(app) {
+    app.get('/administration/bank-holidays',
+      'administration.bank-holidays.get',
+      auth.verify,
+      isSystemAdministrator,
+      controller.getBankHolidays(app),
+    );
+  };
+})();
