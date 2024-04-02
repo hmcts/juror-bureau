fce32a54ff2e09fd2d8491221b8cf7f7a759c3f5
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-04-02 09:58:51 +0100

    integrate axios to start using with the application for new api requests (#104)
    * intrgrate axios to start using with the application for new api requests
    
    * downgrade axios to more stable version
    
    * update axios to latest version
    
    * remove grunt-accessibility
    
    * update browser-sync to latest
    
    * remove browser-syn

--- server/components/logger/index.js
+++ server/components/logger/index.js
@@ -3,77 +3,59 @@
 ;(function(){
   'use strict';
 
-  var winston = require('winston')
-    , utils = require('../../lib/utils')
-    , customColors = {
-      trace: 'white',
-      debug: 'green',
-      info: 'green',
-      warn: 'yellow',
-      crit: 'red',
-      fatal: 'red'
-    }
-    , levels = {
-      fatal: 0,
-      crit: 1,
-      warn: 2,
-      info: 3,
-      debug: 4,
-      trace: 5
-    };
-
-  module.exports = function(config) {
-
-    var origLog
-      , args
-      , transports
-      , loggerInst;
-
-    // Configure our custom logger
-    transports = [
-      new (require('./juror-transport'))({
-        level: 'trace',
-      }),
-    ];
-
-
-    if (config.logConsole) {
-      transports.push(new(winston.transports.Console)({
-        level: config.logConsole,
-        colorize: true,
-        timestamp: true
-      }));
-    }
-
-
-    // Setup the actual logger
-    loggerInst = new(winston.Logger)({
-      colors: customColors,
-      levels: levels,
-      transports: transports
-    });
-
-    winston.addColors(customColors);
-
-    // Extend logger object to properly log 'Error' types
-    origLog = loggerInst.log;
-
-    loggerInst.log = function(level, msg) {
-      if (msg instanceof Error) {
-        args = Array.prototype.slice.call(arguments);
-        args[1] = msg.stack;
-        origLog.apply(loggerInst, args);
-      } else {
-        origLog.apply(loggerInst, arguments);
-      }
-    };
-
-    // Ensure log directory exists
-    utils.checkDirectoryCreate(config.logPath);
-
-    // Return the logger instance to be used with require
-    return loggerInst;
-
+  const winston = require('winston');
+  const { checkDirectoryCreate } = require('../../lib/utils');
+  const customColors = {
+    trace: 'white',
+    debug: 'green',
+    info: 'green',
+    warn: 'yellow',
+    crit: 'red',
+    fatal: 'red',
+  };
+  const levels = {
+    fatal: 0,
+    crit: 1,
+    warn: 2,
+    info: 3,
+    debug: 4,
+    trace: 5,
   };
 
+  module.exports.Logger = class Logger {
+    constructor(config) {
+      this.config = config;
+
+      this.transports = [];
+      this.origLog = null;
+      this.args = null;
+    }
+
+    initLogger(app) {
+      if (this.config.logConsole) {
+        this.transports.push(new(winston.transports.Console)({
+          level: this.config.logConsole,
+          colorize: true,
+          timestamp: true,
+        }));
+      }
+
+      // Setup the actual logger
+      Logger.instance = new(winston.Logger)({
+        colors: customColors,
+        levels: levels,
+        transports: this.transports,
+      });
+
+      winston.addColors(customColors);
+
+      // Ensure log directory exists
+      checkDirectoryCreate(this.config.logPath);
+
+      if (app) {
+        // attach to the app object
+        app.logger = Logger.instance;
+      }
+    }
+  };
 })();
