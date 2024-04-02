847585ba289f4e9d88b9e107c443d78276816852
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-04-02 18:21:27 +0100

    fix conflicts

--- server/components/logger/index.js
+++ server/components/logger/index.js
@@ -1,61 +1,51 @@
-/// Based on this gist: https://gist.github.com/rtgibbons/7354879
+const winston = require('winston');
+const customColors = {
+  trace: 'white',
+  debug: 'green',
+  info: 'green',
+  warn: 'yellow',
+  crit: 'red',
+  fatal: 'red',
+};
+const levels = {
+  fatal: 0,
+  crit: 1,
+  warn: 2,
+  info: 3,
+  debug: 4,
+  trace: 5,
+};
 
-;(function(){
-  'use strict';
+module.exports.Logger = class Logger {
+  constructor (config) {
+    this.config = config;
 
-  const winston = require('winston');
-  const { checkDirectoryCreate } = require('../../lib/utils');
-  const customColors = {
-    trace: 'white',
-    debug: 'green',
-    info: 'green',
-    warn: 'yellow',
-    crit: 'red',
-    fatal: 'red',
-  };
-  const levels = {
-    fatal: 0,
-    crit: 1,
-    warn: 2,
-    info: 3,
-    debug: 4,
-    trace: 5,
-  };
+    this.transports = [];
+    this.origLog = null;
+    this.args = null;
+  }
 
-  module.exports.Logger = class Logger {
-    constructor(config) {
-      this.config = config;
-
-      this.transports = [];
-      this.origLog = null;
-      this.args = null;
+  initLogger (app) {
+    if (this.config.logConsole) {
+      this.transports.push(new (winston.transports.Console)({
+        level: this.config.logConsole,
+        colorize: true,
+        timestamp: true,
+      }));
     }
 
-    initLogger(app) {
-      if (this.config.logConsole) {
-        this.transports.push(new(winston.transports.Console)({
-          level: this.config.logConsole,
-          colorize: true,
-          timestamp: true,
-        }));
-      }
+    // Setup the actual logger
+    Logger.instance = new (winston.Logger)({
+      colors: customColors,
+      levels: levels,
+      transports: this.transports,
+    });
 
-      // Setup the actual logger
-      Logger.instance = new(winston.Logger)({
-        colors: customColors,
-        levels: levels,
-        transports: this.transports,
-      });
+    winston.addColors(customColors);
 
-      winston.addColors(customColors);
-
-      // Ensure log directory exists
-      checkDirectoryCreate(this.config.logPath);
-
-      if (app) {
-        // attach to the app object
-        app.logger = Logger.instance;
-      }
+    if (app) {
+      // attach to the app object
+      app.logger = Logger.instance;
     }
-  };
-})();
+  }
+};
