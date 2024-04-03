1125db02deabbcdd269cc1c748c67a72c2d7d6f0
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-04-03 12:05:27 +0100

    JM-6694: Print a next attendance date report (#131)
    * add first printable report
    
    * fix pool-search results table
    
    * fix postcode capitalization

---
+++ server/routes/reporting/standard-report/utils.js
@@ -0,0 +1,50 @@
+/* eslint-disable strict */
+const { dateFilter, capitalizeFully } = require('../../../components/filters');
+
+// type IReportKey = {[key:string]: {
+//   title: string,
+//   apiKey: string,
+//   search?: 'poolNumber' | 'dateRange', // etc only poolNumber is currently implemented
+//   headings: string[], // corresponds to the ids provided for the headings in the API
+//                       // (except report created dateTime)
+// }};
+module.exports.reportKeys = {
+  'next-due': {
+    title: 'Next attendance date report',
+    apiKey: 'NextAttendanceDayReport',
+    search: 'poolNumber',
+    headings: [
+      'poolNumber',
+      'reportDate',
+      'poolType',
+      'reportTime',
+      'serviceStartDate',
+      'courtName',
+    ],
+    pageHeadings: {
+      left: ['poolNumber', 'poolType', 'serviceStartDate'],
+      right: ['reportDate', 'reportTime', 'courtName'],
+    },
+  },
+};
+
+module.exports.tableDataMappers = {
+  String: (data) => capitalizeFully(data),
+  LocalDate: (data) => dateFilter(data, 'YYYY-mm-dd', 'ddd D MMM YYYY'),
+};
+
+module.exports.headingDataMappers ={
+  String: (data) => capitalizeFully(data),
+  LocalDate: (data) => dateFilter(data, 'YYYY-mm-dd', 'dddd D MMMM YYYY'),
+  timeFromISO: (data) => {
+    const time = data.split('T')[1].split('.')[0];
+
+    if (time.split(':')[0] === 12) {
+      return time + 'pm';
+    } else if (time.split(':')[0] > 12) {
+      return `${time.split(':')[0] - 12}:${time.split(':').slice(1).join(':')}pm`;
+    }
+
+    return time + 'am';
+  },
+};
