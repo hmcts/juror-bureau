1a9a84300c4dad6094c185e4c2db4a45a6f2c3b5
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-04-09 11:26:49 +0100

    add different date validator

--- server/config/validation/jurors-on-trial.js
+++ server/config/validation/jurors-on-trial.js
@@ -1,5 +1,8 @@
 /* eslint-disable strict */
 
+const validate = require('validate.js');
+const { dateFilter } = require('../../components/filters');
+
 module.exports.jurorsOnTrial = function(body) {
   return {
     attendanceDate: {
@@ -20,18 +23,47 @@ module.exports.jurorsOnTrial = function(body) {
         },
       },
     },
-    differentDate: () => {
-      if (!body.attendanceDate || body.attendanceDate !== 'differentDate') return null;
-
-      return {
-        presence: {
-          allowEmpty: false,
-          message: {
-            details: 'Enter date that you’re confirming attendance for',
-            summary: 'Enter date that you’re confirming attendance for',
-          },
-        },
-      };
+    differentDate: {
+      jurorsOnTrialDifferentDate: {
+        attendanceDate: body.attendanceDate,
+      },
     },
   };
+};
+
+validate.validators.jurorsOnTrialDifferentDate = (value, options, key, attributes) => {
+  if (!attributes.attendanceDate || attributes.attendanceDate !== 'differentDate') return null;
+
+  const message = {
+    summary: '',
+    details: [],
+  };
+
+  if (!value) {
+    message.summary = 'Enter date that you’re confirming attendance for';
+    message.details.push('Enter date that you’re confirming attendance for');
+
+    return message;
+  };
+
+  if (!value.match(/^\d{2}\/\d{2}\/\d{4}$/)) {
+    message.summary = 'Enter a date in the correct format, for example, 31/01/2023';
+    message.details.push('Enter a date in the correct format, for example, 31/01/2023');
+
+    return message;
+  }
+
+  const formattedDate = dateFilter(value, 'DD/MM/YYYY', 'YYYY-MM-DD');
+  const tomorrow = new Date();
+
+  tomorrow.setDate(tomorrow.getDate());
+
+  if (new Date(formattedDate) > tomorrow) {
+    message.summary = 'The date you enter must be today or in the past';
+    message.details.push('The date you enter must be today or in the past');
+
+    return message;
+  }
+
+  return null;
 };
