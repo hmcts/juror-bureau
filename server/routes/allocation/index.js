0d01756951302decf746168246a55ebd2565c8c0
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-04-16 10:48:22 +0100

    update the manage users to point to 'bureau admin panel -> users' and update isTeamLead checks (#207)

--- server/routes/allocation/index.js
+++ server/routes/allocation/index.js
@@ -1,29 +1,26 @@
-;(function(){
-  'use strict';
+/* eslint-disable strict */
+const controller = require('./allocation.controller');
+const auth = require('../../components/auth');
+const { isBureauManager } = require('../../components/auth/user-type');
+const responseCountMiddleware = require('../../objects/responses').object;
 
-  var controller = require('./allocation.controller')
-    , auth = require('../../components/auth')
-    , responseCountMiddleware = require('../../objects/responses').object;
+module.exports = function(app) {
+  app.get(
+    '/new-replies',
+    'allocation.get',
+    auth.verify,
+    isBureauManager,
+    responseCountMiddleware.getCount.bind(app),
+    controller.index(app)
+  );
 
-  module.exports = function(app) {
-    app.get(
-      '/new-replies',
-      'allocation.get',
-      auth.verify,
-      auth.isSupervisor,
-      responseCountMiddleware.getCount.bind(app),
-      controller.index(app)
-    );
+  app.post(
+    '/new-replies',
+    'allocation.post',
+    auth.verify,
+    isBureauManager,
+    responseCountMiddleware.getCount.bind(app),
+    controller.post(app)
+  );
 
-    app.post(
-      '/new-replies',
-      'allocation.post',
-      auth.verify,
-      auth.isSupervisor,
-      responseCountMiddleware.getCount.bind(app),
-      controller.post(app)
-    );
-
-  };
-
-})();
+};
