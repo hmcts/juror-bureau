9c6c4245fb1f9d3a2edfed7e4d749ff2b316094a
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2024-04-17 13:13:59 +0100

    hotfix/JM-6963: Added court auth to trial management routes (#216)
    * Added court auth to trial management routes
    
    * Only show trial management link if court user
    
    ---------
    
    Co-authored-by: Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>

--- server/routes/trial-management/empanel-jury/index.js
+++ server/routes/trial-management/empanel-jury/index.js
@@ -3,30 +3,35 @@
 
   const auth = require('../../../components/auth');
   const controller = require('./empanel-jury.controller');
+  const { isCourtUser } = require('../../../components/auth/user-type');
 
   module.exports = function(app) {
 
     app.get('/trial-management/trials/:trialNumber/:locationCode/empanel/number-of-juors',
       'trial-management.empanel.get',
       auth.verify,
+      isCourtUser,
       controller.getEmpanelAmount(app),
     );
 
     app.post('/trial-management/trials/:trialNumber/:locationCode/empanel/number-of-juors',
       'trial-management.empanel.post',
       auth.verify,
+      isCourtUser,
       controller.postEmpanelAmount(app),
     );
 
     app.get('/trial-management/trials/:trialNumber/:locationCode/empanel/jurors-select',
       'trial-management.empanel.select.get',
       auth.verify,
+      isCourtUser,
       controller.getEmpanelJurors(app),
     );
 
     app.post('/trial-management/trials/:trialNumber/:locationCode/empanel/jurors-select',
       'trial-management.empanel.select.post',
       auth.verify,
+      isCourtUser,
       controller.postEmpanelJurors(app),
     );
   };
