babe69365ea8d2cc16de3236e41e6b1331d42c41
Callum Young <callum.young@cgi.com>
2024-04-17 09:19:35 +0100

    Added court auth to trial management routes

--- server/routes/trial-management/edit-trial/index.js
+++ server/routes/trial-management/edit-trial/index.js
@@ -3,18 +3,21 @@
 
   const auth = require('../../../components/auth');
   const controller = require('./edit-trial.controller');
+  const { isCourtUser } = require('../../../components/auth/user-type');
 
   module.exports = function(app) {
 
     app.get('/trial-management/edit-trial/:trialNumber/:locationCode',
       'trial-management.edit-trial.get',
       auth.verify,
+      isCourtUser,
       controller.getEditTrial(app),
     );
 
     app.post('/trial-management/edit-trial/:trialNumber/:locationCode',
       'trial-management.edit-trial.post',
       auth.verify,
+      isCourtUser,
       controller.postEditTrial(app),
     );
 
