7e1f00deffe5a144729ea17f147b4775472e1054
Ben James <ben.james@hmcts.net>
2024-04-18 17:22:54 +0100

    Added aria-label attribute to all table headers with aria-sort attribute

--- server/routes/documents/helper/table-generator.js
+++ server/routes/documents/helper/table-generator.js
@@ -38,6 +38,7 @@
           scope="col"
           class="govuk-table__header ${numberTypeClass}"
           aria-sort="${sortDirection}"
+          aria-label="${$curr}"
         >
           ${curr}
         </th>
