a4ff1849c124b6e85312ee39210bb1e1f93aef0c
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-04-23 18:14:03 +0100

    update and fix non-attendance-days

--- server/routes/juror-management/expenses/non-attendance-day/index.js
+++ server/routes/juror-management/expenses/non-attendance-day/index.js
@@ -5,12 +5,12 @@
   const controller = require('./non-attendance-day.controller');
 
   module.exports = function(app) {
-    app.get('/juror-management/unpaid-attendance/non-attendance-day/:jurorNumber/:locCode',
+    app.get('/juror-management/unpaid-attendance/non-attendance-day/:jurorNumber/:poolNumber',
       'juror-management.non-attendance-day.get',
       auth.verify,
       controller.getNonAttendanceDay(app));
 
-    app.post('/juror-management/unpaid-attendance/non-attendance-day/:jurorNumber/:locCode',
+    app.post('/juror-management/unpaid-attendance/non-attendance-day/:jurorNumber/:poolNumber',
       'juror-management.non-attendance-day.post',
       auth.verify,
       controller.postNonAttendanceDay(app));
