269eaf02a0edd6ac5a94271376665376199e2fe5
Sohesh Jhuboo <sohesh.jhuboo@cgi.com>
2024-04-23 13:49:21 +0100

    hotfix/JM-7093
    - Given acces for standard court users to view Court Administration page
    - Removed access to Administration Users page for standard court users
    - Limited standard court users to only view Expense limits and not edit

--- server/routes/administration/expense-limits-court/index.js
+++ server/routes/administration/expense-limits-court/index.js
@@ -3,14 +3,14 @@
 
   const auth = require('../../../components/auth');
   const controller = require('./expense-limits-court.controller');
-  const { isCourtManager } = require('../../../components/auth/user-type');
+  const { isCourtManager, isCourtUser } = require('../../../components/auth/user-type');
 
   module.exports = function(app) {
 
     app.get('/administration/expense-limits-court',
       'administration.expense-limits-court.get',
       auth.verify,
-      isCourtManager,
+      isCourtUser,
       controller.getExpenseLimitsCourt(app),
     );
 
--- server/routes/administration/room-locations/index.js
+++ server/routes/administration/room-locations/index.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const auth = require('../../../components/auth');
-  const { isCourtManager } = require('../../../components/auth/user-type');
+  const { isCourtUser } = require('../../../components/auth/user-type');
   const controller = require('./room-locations.controller');
 
   module.exports = function(app) {
@@ -10,35 +10,35 @@
     app.get('/administration/room-locations/:locationCode',
       'administration.room-locations.get',
       auth.verify,
-      isCourtManager,
+      isCourtUser,
       controller.getRoomLocations(app),
     );
 
     app.get('/administration/room-locations/:locationCode/edit/:id',
       'administration.room-locations.edit.get',
       auth.verify,
-      isCourtManager,
+      isCourtUser,
       controller.getEditCourtroom(app),
     );
 
     app.post('/administration/room-locations/:locationCode/edit/:id',
       'administration.room-locations.edit.post',
       auth.verify,
-      isCourtManager,
+      isCourtUser,
       controller.postEditCourtroom(app),
     );
 
     app.get('/administration/room-locations/:locationCode/add',
       'administration.room-locations.add.get',
       auth.verify,
-      isCourtManager,
+      isCourtUser,
       controller.getAddCourtroom(app),
     );
 
     app.post('/administration/room-locations/:locationCode/add',
       'administration.room-locations.add.post',
       auth.verify,
-      isCourtManager,
+      isCourtUser,
       controller.postAddCourtroom(app),
     );
   };
