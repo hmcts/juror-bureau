9f297f7f331de6c902eed6d4de285fd2acca306e
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-05-20 11:14:27 +0100

    remove test helloworld route

--- server/routes/index.js
+++ server/routes/index.js
@@ -45,10 +45,6 @@
     require('./messaging')(app);
     require('./administration')(app);
 
-    app.get('/helloworld', function(req, res) {
-      return res.send('hello world');
-    });
-
     app.route('/health')
       .get(function(req, res) {
         return errors(req, res, 200);
