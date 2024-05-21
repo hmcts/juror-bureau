0a422865e2882efaee6e71c29689cf10b7429150
Hari Vajja <hari.vajja@cgi.com>
2024-05-21 16:26:48 +0100

    jm-6492

--- server/routes/reporting/standard-report/index.js
+++ server/routes/reporting/standard-report/index.js
@@ -61,6 +61,7 @@
     standardReportRoutes(app, 'panel-detail');
     standardReportRoutes(app, 'jury-list');
     standardReportRoutes(app, 'pool-status');
+    standardReportRoutes(app, 'pool-analysis');
     // require('../pool-status')(app);
     standardReportRoutes(app, 'reasonable-adjustments');
     require('../persons-attending')(app);
