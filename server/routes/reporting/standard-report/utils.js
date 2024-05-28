bf666becce40a43ea1763258fee53592c5f87095
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-05-28 13:33:00 +0100

    update the reports table renderer

--- server/routes/reporting/standard-report/utils.js
+++ server/routes/reporting/standard-report/utils.js
@@ -92,6 +92,11 @@ const buildTableHeaders = (reportType, tableHeadings) => {
   } else {
     tableHeaders = tableHeadings.map((data, index) => {
       if (!data.name || data.name === '') return;
+      let classes = '';
+
+      if (data.name === 'Service Start Date') {
+        classes = classes + ' mod-min-width-150';
+      }
 
       return ({
         text: data.name,
@@ -99,7 +104,9 @@ const buildTableHeaders = (reportType, tableHeadings) => {
           'aria-sort': index === 0 ? 'ascending' : 'none',
           'aria-label': data.name,
         },
-        classes: reportType.bespokeReport?.tableHeadClasses ? reportType.bespokeReport?.tableHeadClasses[index] : '',
+        classes: reportType.bespokeReport?.tableHeadClasses
+          ? reportType.bespokeReport?.tableHeadClasses[index]
+          : classes,
         format: data.dataType === 'BigDecimal' ? 'numeric' : '',
       });
     });
