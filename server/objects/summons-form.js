8f5a27ae1afc41440f12940a5de55955395c4b3f
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-06-05 11:06:15 +0100

    add security check for when summoning citizens to prevent two officers from double summoning jurors

--- server/objects/summons-form.js
+++ server/objects/summons-form.js
@@ -1,48 +1,18 @@
-;(function() {
-  'use strict';
+const { DAO } = require('./dataAccessObject');
+const { dateFilter } = require('../components/filters');
 
-  var _ = require('lodash')
-    , urljoin = require('url-join')
-    , config = require('../config/environment')()
-    , utils = require('../lib/utils')
-    , options = {
-      uri: config.apiEndpoint,
-      headers: {
-        'User-Agent': 'Request-Promise',
-        'Content-Type': 'application/vnd.api+json'
-      },
-      json: true,
-      transform: utils.basicDataTransform,
-    }
-    , dateFilter = require('../components/filters').dateFilter
+module.exports.summonsFormDAO = new DAO('moj/pool-create/summons-form', {
+  post: function(pd) {
+    const nextDate = dateFilter(new Date(pd.poolDetails.courtStartDate), null, 'YYYY-MM-DD');
 
-    , poolSummaryObject = {
-      resource: 'moj/pool-create/summons-form',
-      post: function(rp, app, jwtToken, pd) {
-        var reqOptions = _.clone(options)
-          , tmpBody = {};
+    const body = {
+      poolNumber: pd.poolDetails.poolNumber,
+      nextDate,
+      catchmentArea: pd.currentCatchmentArea,
+      attendTime: nextDate + ' 00:00',
+      noRequested: pd.bureauSummoning.required,
+    };
 
-        tmpBody.poolNumber = pd.poolDetails.poolNumber;
-        tmpBody.nextDate = dateFilter(new Date(pd.poolDetails.courtStartDate), null, 'YYYY-MM-DD');
-        tmpBody.catchmentArea = pd.currentCatchmentArea;
-        tmpBody.attendTime = tmpBody.nextDate + ' 00:00';
-        tmpBody.noRequested = pd.bureauSummoning.required;
-
-        reqOptions.headers.Authorization = jwtToken;
-        reqOptions.uri = urljoin(reqOptions.uri, this.resource);
-        reqOptions.method = 'POST';
-        reqOptions.body = tmpBody;
-
-        app.logger.debug('Sending request to API: ', {
-          uri: reqOptions.uri,
-          headers: reqOptions.headers,
-          method: reqOptions.method,
-          data: reqOptions.body,
-        });
-
-        return rp(reqOptions);
-      },
-    }
-
-  module.exports.poolSummaryObject = poolSummaryObject;
-})();
+    return { body };
+  },
+});
