3153ee44dedbf08cbc696ba6d263227a41a471f9
Callum Young <callum.young@cgi.com>
2024-06-07 13:33:52 +0100

    print audit report links

--- server/routes/reporting/standard-report/standard-report.controller.js
+++ server/routes/reporting/standard-report/standard-report.controller.js
@@ -330,11 +330,11 @@
           if (header.id === 'attendance_audit' && output !== '-') {
             let link;
             if (output && output.charAt(0) === 'P') {
-              link = app.namedRoutes.build('reports.pool-attendance-audit.report.get', {
+              link = app.namedRoutes.build('reports.pool-attendance-audit.report.print', {
                 filter: output,
               })
             } else if (output && output.charAt(0) === 'J') {
-              link = app.namedRoutes.build('reports.jury-attendance-audit.report.get', {
+              link = app.namedRoutes.build('reports.jury-attendance-audit.report.print', {
                 filter: output,
               })
             }
