f3771969a3fc418e54995410ec56adc5c98834b9
Ricardo <140706972+ricardofreitasrocha@users.noreply.github.com>
2024-07-31 13:11:56 +0100

    Application crashing on trying to print letters (#675)
    * add redirect for document-list if the documentsJurorList is not present when posting a print action
    
    * add some logging to crash prevention

--- server/routes/documents/letters-list/letters-list.controller.js
+++ server/routes/documents/letters-list/letters-list.controller.js
@@ -113,6 +113,14 @@
       const { document } = req.params;
       const { origin } = req.query;
 
+      if (!req.session.documentsJurorsList) {
+        app.logger.warn('No document jurors list found. Redirecting back to document-type list page', {
+          auth: req.session.authentication,
+        });
+
+        return res.redirect(app.namedRoutes.build('documents.get'));
+      }
+
       if (origin === 'statusChanged') {
         removeStatusChangedJurors(req);
       }
