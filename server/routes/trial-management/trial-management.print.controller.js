82acbdbc5a987c10c3a2c77388b652949ba17965
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2024-08-27 10:56:56 +0100

    allow printing of the trials list on the trial-list page

---
+++ server/routes/trial-management/trial-management.print.controller.js
@@ -0,0 +1,83 @@
+const { mapCamelToSnake, camelToSnake } = require('../../lib/mod-utils');
+const { capitalise, dateFilter, makeDate } = require('../../components/filters');
+const { trialsListDAO } = require('../../objects');
+const { generateDocument } = require('../../lib/reports/single-generator');
+
+module.exports.getPrintTrials = (app) => {
+  return async (req, res) => {
+    const { isActive, sortBy, sortOrder } = req.query;
+
+    const opts = {
+      active: isActive || 'true',
+      pageNumber: 1,
+      pageLimit: 500,
+      sortField: capitalise(camelToSnake(sortBy || 'startDate')),
+      sortMethod: sortOrder === 'descending' ? 'DESC' : 'ASC',
+    };
+
+    let response;
+
+    try {
+      response = await trialsListDAO.post(req, mapCamelToSnake(opts));
+    } catch (err) {
+      app.logger.crit('Failed to get trials list', {
+        auth: req.session.authentication,
+        data: opts,
+        error: (typeof err.error !== 'undefined') ? err.error : err.toString(),
+      });
+
+      return res.render('_errors/generic');
+    }
+
+    const documentContent = buildPdfTable(response.data, response.total_items, isActive === 'false');
+
+    let document;
+    try {
+      document = await generateDocument(documentContent);
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+
+    res.contentType('application/pdf');
+    res.send(document);
+  };
+}
+
+function buildPdfTable(data, amount, includeInactive) {
+  const body = data.map((trial) => {
+    return [
+      { text: trial.trial_number },
+      { text: trial.parties },
+      { text: trial.trial_type },
+      { text: trial.court },
+      { text: trial.courtroom },
+      { text: trial.judge },
+      { text: dateFilter(makeDate(trial.start_date), 'YYYY,MM,DD', 'ddd DD MMM YYYY') },
+    ];
+  });
+
+  return {
+    metadata: {
+      left: [
+        { key: 'Trials', value: amount },
+        { key: 'Include inactive', value: includeInactive ? 'Yes' : 'No' },
+      ],
+      right: [],
+    },
+    tables: [
+      {
+        head: [
+          { text: 'Case number', style: 'label' },
+          { text: 'Names', style: 'label' },
+          { text: 'Trial type', style: 'label' },
+          { text: 'Court', style: 'label' },
+          { text: 'Courtroom', style: 'label' },
+          { text: 'Judge', style: 'label' },
+          { text: 'Start date', style: 'label' },
+        ],
+        body,
+      },
+    ],
+  }
+}
