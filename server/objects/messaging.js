9c819679a7a799dbc22d42a1a921ddcc5ebd80e5
Callum Young <callum.young@cgi.com>
2024-10-28 15:50:47 +0000

    Update messaging.js to axios

--- server/objects/messaging.js
+++ server/objects/messaging.js
@@ -1,61 +1,24 @@
 (function() {
   'use strict';
 
-  const _ = require('lodash')
-    , config = require('../config/environment')()
-    , options = {
-      uri: config.apiEndpoint,
-      headers: {
-        'User-Agent': 'Request-Promise',
-        'Content-Type': 'application/vnd.api+json',
-      },
-      json: true,
-    };
-
+  const _ = require('lodash');
   const urljoin = require('url-join');
-  const rp = require('request-promise');
   const { DAO } = require('./dataAccessObject');
 
-  module.exports.messageTemplateDAO = {
-    get: function(app, req, messageType, locCode) {
-      const payload = {
-        uri: urljoin(config.apiEndpoint, 'moj/messages/view', messageType, locCode),
-        method: 'GET',
-        headers: {
-          Authorization: req.session.authToken,
-          'Content-Type': 'application/vnd.api+json',
-        },
-        json: true,
-      };
+  module.exports.messageTemplateDAO = new DAO('moj/messages/view', {
+    get: function(messageType, locCode) {
+      return { uri: urljoin(this.resource, messageType, locCode) };
+    }
+  });
 
-      app.logger.info('Sending request to API: ', payload);
-
-      return rp(payload);
-    },
-  };
-
-  module.exports.populatedMessageDAO = {
-    post: function(app, req, messageType, locCode, body) {
-      const payload = {
-        uri: urljoin(config.apiEndpoint,
-          'moj/messages/view/{messageType}/{locCode}/populated'
-            .replace('{messageType}', messageType)
-            .replace('{locCode}', locCode)
-        ),
-        method: 'POST',
-        headers: {
-          Authorization: req.session.authToken,
-          'Content-Type': 'application/vnd.api+json',
-        },
-        json: true,
+  module.exports.populatedMessageDAO = new DAO('moj/messages/view/{messageType}/{locCode}/populated', {
+    post: function(messageType, locCode, body) {
+      return {
+        uri: this.resource.replace('{messageType}', messageType).replace('{locCode}', locCode),
         body,
-      };
-
-      app.logger.info('Sending request to API: ', payload);
-
-      return rp(payload);
-    },
-  };
+      }
+    }
+  });
 
   module.exports.jurorSearchDAO = new DAO('moj/messages/search', {
     post: function(locCode, _body, simpleResponse) {
@@ -77,56 +40,26 @@
     }
   });
 
-  const sendMessage = {
-    resource: 'moj/messages/send',
-
-    post: function(app, jwtToken, messageType, locCode, body) {
-      const reqOptions = _.clone(options);
-      let tmpBody = _.clone(body);
-
-      reqOptions.headers.Authorization = jwtToken;
-      reqOptions.method = 'POST';
-      reqOptions.uri = urljoin(
-        reqOptions.uri,
-        this.resource,
-        messageType,
-        locCode,
-      );
-
-      tmpBody = _.mapKeys(tmpBody, (__, key) => _.snakeCase(key));
-
-      reqOptions.body = tmpBody;
-
-      app.logger.debug('Sending request to API: ', {
-        uri: reqOptions.uri,
-        headers: reqOptions.headers,
-        method: reqOptions.method,
-        body: reqOptions.body,
-      });
-
-      return rp(reqOptions);
-    },
-  };
-
-  module.exports.downloadCSVDAO = {
-    post: function(app, req, locCode, body) {
-      const payload = {
-        uri: urljoin(config.apiEndpoint, 'moj/messages/csv', locCode),
-        method: 'POST',
-        headers: {
-          Authorization: req.session.authToken,
-          'Content-Type': 'application/json',
-          'Accept': 'text/csv, application/json',
-        },
-        json: true,
-        body,
+  module.exports.sendMessage = new DAO('moj/messages/send', {
+    post: function(messageType, locCode, body) {
+      return { 
+        uri: urljoin(this.resource, messageType, locCode),
+        body: _.mapKeys(body, (__, key) => _.snakeCase(key))
       };
+    }
+  });
 
-      app.logger.info('Sending request to API: ', payload);
+  module.exports.downloadCSVDAO = new DAO('moj/messages/csv', {
+    post: function(locCode, body) {
+      return {
+        uri: urljoin(this.resource, locCode),
+        body,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        transform: (data) => { console.log(data); delete data._headers; return data.data; }
+      };
+    }
+  });
 
-      return rp(payload);
-    },
-  };
-
-  module.exports.sendMessage = sendMessage;
 })();
