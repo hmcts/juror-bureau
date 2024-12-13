a5856d9f7115f190cb5ae7d330b392c504105d72
Callum Young <callum.young@cgi.com>
2024-12-13 10:58:26 +0000

    Update account holder name validation

--- server/config/validation/bank-details.js
+++ server/config/validation/bank-details.js
@@ -120,10 +120,10 @@
           details: 'Account holder\'s name must be 18 characters or fewer',
         };
       }
-      if (/[.,"]/.test(value)) {
+      if (/[^a-zA-Z0-9 ./&-]/.test(value)) {
         tmpErrors = {
-          summary: 'Account holder\'s name cannot include full stops, commas or double quote marks',
-          details: 'Account holder\'s name cannot include full stops, commas or double quote marks',
+          summary: 'Account holder\'s name can only include letters, numbers, spaces, commas, hyphens, forward slash, and ampersands',
+          details: 'Account holder\'s name can only include letters, numbers, spaces, commas, hyphens, forward slash, and ampersands',
         };
       }
     }
