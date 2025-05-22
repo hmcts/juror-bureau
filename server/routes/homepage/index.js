f806a9ce90cacffd464284dbb45334be839ad5cb
Callum Young <callum.young@cgi.com>
2025-05-22 13:32:56 +0100

    Initial prototype

--- server/routes/homepage/index.js
+++ server/routes/homepage/index.js
@@ -5,7 +5,7 @@
     , auth = require('../../components/auth');
 
   module.exports = function(app) {
-    app.get('/homepage', 'homepage.get', auth.verify, controller.homepage(app));
+    app.get('/homepage', 'homepage.get', auth.verify, controller.dashboard(app));
   };
 
 })();
---
+++ server/stores/court-dashboard.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+  attendanceStatsToday: {
+    expected: 162,
+    checkedIn: 68,
+    onTrials: 23,
+    notCheckedIn: 71
+  },
+  attendanceStatsLast7Days: {
+    expected: 1134,
+    checkedIn: 1000,
+    onTrials: 100,
+    notCheckedIn: 34
+  },
+  attendanceStatsNext7Days: {
+    totalDueToAttend: 350,
+    totalReasonableAdjustments: 58,
+  },
+  unconfirmedAttendances: {
+    totalUnconfirmed: 20,
+  },
+  monthlyUtilisationStats: {
+    lastDate: '2025-04-28',
+  },
+  unpaidAttendances: {
+    unpaidAttendances: 16,
+    oldestUnpaidDate: '2025-03-01',
+  },
+}
