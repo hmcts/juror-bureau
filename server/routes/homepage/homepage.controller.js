021e0f12e3a064b4cf63821978779b77bcddc09c
Callum Young <callum.young@cgi.com>
2025-06-04 10:43:06 +0100

    Integrate notifications endpoint

--- server/routes/homepage/homepage.controller.js
+++ server/routes/homepage/homepage.controller.js
@@ -22,50 +22,13 @@ const { notifications } = require('../../stores/court-dashboard');
     };
   };
 
-  // Maybe could be replaced with dedicated notifcations api endpoint - to discuss with BE
-  async function getNotificationsHTML(app, req, res) {
-    let notifications = [];
-
-    if (isSJOUser(req)) {
-      try {
-        const approvals = await jurorsForApproval.get(
-          req,
-          req.session.authentication.locCode,
-          'QUEUED'
-        );
-
-        const noApprovals = approvals.pending_jurors_response_data.length;
-
-        if (noApprovals !== 0) {
-          req.session.jurorApprovalCount = noApprovals;
-          notifications.push(
-            '<p class="govuk-body">You have ' +
-            noApprovals +
-            ' <a href="/juror-management/manage-jurors/approve">' +
-            ` juror${noApprovals > 1 ? 's' : ''}` +
-            ' to approve</a></p>');
-        }
-      } catch (err) {
-        app.logger.crit('Unable to fetch pending juror list', {
-          auth: req.session.authentication,
-          token: req.session.authToken,
-          data: {
-            locationCode: req.session.authentication.owner,
-            status: 'QUEUED',
-          },
-          error: typeof err.error !== 'undefined' ? err.error : err.toString(),
-        });
-      }
-    }
-    return notifications;
-  }
-
   module.exports.dashboard = function(app) {
     return async function(req, res) {
 
       let notifcations;
       try {
         notifcations = await dashboardNotifications.get(req, req.session.authentication.locCode);
+        console.log('\n\nnotifications', JSON.stringify(notifcations, null, 2), '\n\n');
       } catch (err) {
         app.logger.crit('Unable to fetch notifications', {
           auth: req.session.authentication,
@@ -86,7 +49,6 @@ const { notifications } = require('../../stores/court-dashboard');
       }
       console.log('\n\widgets\n', JSON.stringify(widgets, null, 2), '\n\n');
 
-      console.log('notifcations', buildDashboardNotifications(app)(req, res)(notifcations),);
       res.render('homepage/court-dashboard/dashboard.njk', {
         notifications: buildDashboardNotifications(app)(req, res)(notifcations),
         widgets,
@@ -99,12 +61,17 @@ const { notifications } = require('../../stores/court-dashboard');
     const notifcationList = [];
     for (const [notificationType, value] of Object.entries(notifcations)) {
       let notificationHTML;
-      switch (notificationType) {
-        case 'urgentReplies':
-          notificationHTML = value > 0 ? `You have <b>${value}</b> <a href="#" class="govuk-link">summons replies</a> with less than a week until service start date to process.` : null;
-          break;
+      if (value > 0) {
+        switch (notificationType) {
+          case 'openSummonsReplies':
+            notificationHTML = `You have <b>${value}</b> <a href="#" class="govuk-link">summons replies</a> with less than a week until service start date to process.`;
+            break;
+          case 'pendingJurors':
+            notificationHTML = `You have <b>${value}</b> <a href="/juror-management/manage-jurors/approve"> juror${value > 1 ? 's' : ''} to approve</a>`
+            break;
+        }
+        notifcationList.push(notificationHTML);
       }
-      if (notificationHTML) notifcationList.push(notificationHTML);
     }
 
     return notifcationList;
