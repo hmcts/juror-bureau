85045566794f12c567e792383222e233b8d5eb9d
Callum Young <callum.young@cgi.com>
2025-06-09 16:51:55 +0100

    Update attendance doughnut charts

--- server/stores/court-dashboard.js
+++ server/stores/court-dashboard.js
@@ -1,24 +1,23 @@
 
 module.exports = {
-  attendanceStats: {
-    today: {
-      expected: 162,
-      checkedIn: 68,
-      onTrials: 23,
-      notCheckedIn: 71
-    },
-    last7Days: {
-      expected: 1134,
-      checkedIn: 1000,
-      onTrials: 100,
-      notCheckedIn: 34
-    },
-    next7Days: {
-      totalDueToAttend: 350,
-      totalReasonableAdjustments: 58,
-    },
-    unconfirmedAttendances: 20
+  attendanceStatsToday: {
+    expected: 162,
+    checkedIn: 65,
+    checkedOut: 3,
+    onTrials: 23,
+    notCheckedIn: 71
   },
+  attendanceStatsLastSevenDays: {
+    expected: 1134,
+    attended: 1000,
+    onTrials: 100,
+    absent: 34
+  },
+  next7Days: {
+    totalDueToAttend: 350,
+    totalReasonableAdjustments: 58,
+  },
+  unconfirmedAttendances: 20,
   adminStats: {
     lastMonthlyUtilisationDate: '2025-04-28',
   },
