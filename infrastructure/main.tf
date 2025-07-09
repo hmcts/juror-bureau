55b05daf60ebc99f069690a162cae2464b38b8c4
Neil Perry <neil.perry@cgi.com>
2025-07-09 12:03:24 +0100

    correct module url

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -56,6 +56,6 @@ resource "azurerm_monitor_diagnostic_setting" "cache-ds" {
 }
 
 module "log_analytics_workspace" {
-  source      = "git::https://github.com/hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  source      = "git@github.com:hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
   environment = var.env
 }
