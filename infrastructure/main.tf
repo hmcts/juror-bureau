fc429c2023470b0e90e0575b11ff99df8e82aa28
Neil Perry <142518199+npv0@users.noreply.github.com>
2025-07-31 15:10:25 +0100

    add redis cache audit logs (#969)
    * add redis cache audit logs
    
    * correct module url
    
    * correct resource_group_name
    
    * further update data
    
    * Update yarn-audit-knwn-issues
    
    * update to correct redis categories
    
    ---------
    
    Co-authored-by: Christopher Powell (CGI) <151015663+ChrisPowellcgi@users.noreply.github.com>
    Co-authored-by: akikrahman1 <149579836+akikrahman1@users.noreply.github.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,11 +5,12 @@ provider "azurerm" {
 locals {
   resourceName = "${var.product}-${var.env}-rg"
   vaultName    = "${var.product}-${var.env}"
+  cacheName    = "${var.product}-${var.component}-redis-cache"
 }
 
 module "juror-bureau-redis" {
   source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product                       = "${var.product}-${var.component}-redis-cache"
+  product                       = local.cacheName
   location                      = var.location
   env                           = var.env
   common_tags                   = var.common_tags
@@ -31,4 +32,34 @@ resource "azurerm_key_vault_secret" "redis_connection_string" {
   name         = "${var.component}-redisConnection"
   value        = "rediss://:${urlencode(module.juror-bureau-redis.access_key)}@${module.juror-bureau-redis.host_name}:${module.juror-bureau-redis.redis_port}"
   key_vault_id = data.azurerm_key_vault.juror.id
+}
+
+data "azurerm_redis_cache" "juror" {
+  name                = "${local.cacheName}-${var.env}"
+  resource_group_name = "${local.cacheName}-cache-${var.env}"
+}
+
+resource "azurerm_monitor_diagnostic_setting" "cache-ds" {
+  name                       = "${local.cacheName}-${var.env}"
+  target_resource_id         = data.azurerm_redis_cache.juror.id
+  log_analytics_workspace_id = module.log_analytics_workspace.workspace_id
+
+  enabled_log {
+    category = "ConnectedClientList"
+  }
+
+  enabled_log {
+    category = "MSEntraAuthenticationAuditLog"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      metric,
+    ]
+  }
+}
+
+module "log_analytics_workspace" {
+  source      = "git@github.com:hmcts/terraform-module-log-analytics-workspace-id.git?ref=master"
+  environment = var.env
 }
