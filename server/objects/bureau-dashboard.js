79842373ade7ac2c258cd654cced45814cc70dad
Christopher Powell (CGI) <151015663+ChrisPowellcgi@users.noreply.github.com>
2025-08-04 11:18:47 +0100

    JS-414 - Bureau homepage dashboard (#997)
    * JS-414 - Bureau homepage dashboard
    
    * Update notifications endpoint URL / definitions

---
+++ server/objects/bureau-dashboard.js
@@ -0,0 +1,29 @@
+const dataStore = require('../stores/court-dashboard');
+
+;(function() {
+  'use strict';
+
+  const { DAO } = require('./dataAccessObject');
+  const urljoin = require('url-join');
+  const { replaceAllObjKeys } = require('../lib/mod-utils');
+  const _ = require('lodash');
+
+  module.exports.bureauDashboardDAO = new DAO('moj/bureau-dashboard', {
+    get: function(section) {
+      return {
+        uri: urljoin(this.resource, section),
+        transform: (data) => { delete data['_headers']; return replaceAllObjKeys(data, _.camelCase) },
+      };
+    }
+  })
+
+  module.exports.bureauDashboardPoolsUnderResponded = new DAO('moj/bureau-dashboard/pools-under-responded', {
+     get: function(params) {
+      return {
+        uri: urljoin(this.resource, params),
+        transform: (data) => { delete data['_headers']; return replaceAllObjKeys(data, _.camelCase) },
+      };
+    }
+  })
+
+})();
---
+++ server/stores/bureau-dashboard.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  summonsYourWork: {
+    total: 1134,
+    standard: 768,
+    overdue: 12,
+    fourWeeks: 354
+  },
+  summonsAssignReplies: {
+    assigned: 1005,
+    unassigned: 132
+  },
+}
