7052d422d1d5cdd26adb987f5d96b2bd76ec3557
Callum Young <callum.young@cgi.com>
2025-10-22 11:16:32 +0100

    Update sass-loader to v16.0.5

--- webpack/scss.js
+++ webpack/scss.js
@@ -25,6 +25,7 @@ module.exports = {
         {
           loader: 'sass-loader',
           options: {
+            api: 'legacy',
             sassOptions: {
               quietDeps: true,
             },
