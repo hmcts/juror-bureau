f88fd2416f8b7060081a5029692eb672ed9be45f
Callum Young <callum.young@cgi.com>
2025-11-03 17:05:31 +0000

    Refactor summons update controllers

--- server/routes/summons-management/update/eligibility.controller.js
+++ server/routes/summons-management/update/eligibility.controller.js
@@ -9,122 +9,100 @@
   const paperReplyValidator = require('../../../config/validation/paper-reply');
   const { validate } = require('validate.js');
 
-  module.exports.get = function(app) {
-    return async function(req, res) {
-      const { id } = req.params;
-      const postUrl = app.namedRoutes.build('summons.update-eligibility.post', {
-        id: req.params['id'],
-        type: req.params['type'],
+  module.exports.get = (app) => async (req, res) => {
+    const { id, type } = req.params;
+    const postUrl = app.namedRoutes.build('summons.update-eligibility.post', { id, type });
+    const cancelUrl = app.namedRoutes.build('response.paper.details.get', { id, type: 'paper' }) + '#eligibility';
+    const tmpErrors = _.clone(req.session.errors);
+    const tmpBody = _.clone(req.session.formFields);
+
+    delete req.session.errors;
+    delete req.session.formFields;
+
+    try {
+      const { headers, data } = await paperReplyObj.get(
+        req,
+        id
+      );
+
+      req.session[`summonsUpdate-${id}`] = {
+        etag: headers['etag'],
+      };
+
+      return res.render('summons-management/paper-reply/eligibility.njk', {
+        postUrl,
+        cancelUrl,
+        eligibilityDetails: tmpBody || getEligibilityDetails(data.eligibility),
+        errors: {
+          title: 'Please check the form',
+          count: typeof tmpErrors !== 'undefined' ? Object.keys(tmpErrors).length : 0,
+          items: tmpErrors,
+        },
       });
-      const cancelUrl = app.namedRoutes.build('response.paper.details.get', {
-        id: req.params['id'],
-        type: 'paper',
-      }) + '#eligibility';
-      const tmpErrors = _.clone(req.session.errors);
-      const tmpBody = _.clone(req.session.formFields);
+    } catch (err) {
+      app.logger.crit('Unable to fetch the summons details', {
+        auth: req.session.authentication,
+        data: {
+          id,
+        },
+        error: typeof err.error !== 'undefined' ? err.error : err.toString(),
+      });
 
-      delete req.session.errors;
-      delete req.session.formFields;
-
-      try {
-        const { headers, data } = await paperReplyObj.get(
-          req,
-          req.params['id']
-        );
-
-        req.session[`summonsUpdate-${id}`] = {
-          etag: headers['etag'],
-        };
-
-        return res.render('summons-management/paper-reply/eligibility.njk', {
-          postUrl,
-          cancelUrl,
-          eligibilityDetails: tmpBody || getEligibilityDetails(data.eligibility),
-          errors: {
-            title: 'Please check the form',
-            count: typeof tmpErrors !== 'undefined' ? Object.keys(tmpErrors).length : 0,
-            items: tmpErrors,
-          },
-        });
-      } catch (err) {
-        app.logger.crit('Unable to fetch the summons details', {
-          auth: req.session.authentication,
-          data: {
-            id: req.params['id'],
-          },
-          error: typeof err.error !== 'undefined' ? err.error : err.toString(),
-        });
-
-        return res.render('_errors/generic', { err });
-      }
-    };
+      return res.render('_errors/generic', { err });
+    }
   };
 
-  module.exports.post = function(app) {
-    return async function(req, res) {
-      const { id } = req.params;
+  module.exports.post = (app) => async (req, res) => {
+    const { id, type } = req.params;
 
-      const eligibility = createEligibilityObject(req.body);
+    const eligibility = createEligibilityObject(req.body);
 
-      const validatorResult = validate(req.body, paperReplyValidator.eligibility());
-      if (typeof validatorResult !== 'undefined') {
-        req.session.errors = validatorResult;
-        req.session.formFields = req.body;
+    const validatorResult = validate(req.body, paperReplyValidator.eligibility());
+    if (typeof validatorResult !== 'undefined') {
+      req.session.errors = validatorResult;
+      req.session.formFields = req.body;
 
-        return res.redirect(app.namedRoutes.build('summons.update-eligibility.get', {
-          id: req.params['id'],
-          type: 'paper',
-        }));
+      return res.redirect(app.namedRoutes.build('summons.update-eligibility.get', { id, type: 'paper' }));
+    }
+
+    mergeMentalHealthInfo(eligibility);
+
+    try {
+      const wasModified = await hasBeenModified(app, req);
+
+      if (wasModified) {
+        return res.redirect(app.namedRoutes.build('summons.update-eligibility.get', { id, type: 'paper' }));
       }
 
-      mergeMentalHealthInfo(eligibility);
+      await summonsUpdate.patch(
+        req,
+        id,
+        'ELIGIBILITY',
+        {
+          eligibility,
+        },
+      );
 
-      try {
-        const wasModified = await hasBeenModified(app, req);
+      delete req.session[`summonsUpdate-${id}`];
 
-        if (wasModified) {
-          return res.redirect(app.namedRoutes.build('summons.update-eligibility.get', {
-            id: req.params['id'],
-            type: 'paper',
-          }));
-        }
+      app.logger.info('Successfully updated the juror eligibility: ', {
+        auth: req.session.authentication,
+        data: {
+          summonsId: id,
+        },
+      });
 
-        await summonsUpdate.patch(
-          req,
-          req.params['id'],
-          'ELIGIBILITY',
-          {
-            eligibility,
-          },
-        );
+      return res.redirect(app.namedRoutes.build('response.paper.details.get', { id, type: 'paper' }) + '#eligibility');
+    } catch (err) {
+      app.logger.crit('Could not update the summons eligibility details', {
+        auth: req.session.authentication,
+        error: (typeof err.error !== 'undefined') ? err.error : err.toString(),
+      });
 
-        delete req.session[`summonsUpdate-${id}`];
+      generalError(req);
 
-        app.logger.info('Successfully updated the juror eligibility: ', {
-          auth: req.session.authentication,
-          data: {
-            summonsId: req.params['id'],
-          },
-        });
-
-        return res.redirect(app.namedRoutes.build('response.paper.details.get', {
-          id: req.params['id'],
-          type: 'paper',
-        }) + '#eligibility');
-      } catch (err) {
-        app.logger.crit('Could not update the summons eligibility details', {
-          auth: req.session.authentication,
-          error: (typeof err.error !== 'undefined') ? err.error : err.toString(),
-        });
-
-        generalError(req);
-
-        return res.redirect(app.namedRoutes.build('summons.update-eligibility.get', {
-          id: req.params['id'],
-          type: 'paper',
-        }));
-      }
-    };
+      return res.redirect(app.namedRoutes.build('summons.update-eligibility.get', { id, type: 'paper' }));
+    }
   };
 
 })();
