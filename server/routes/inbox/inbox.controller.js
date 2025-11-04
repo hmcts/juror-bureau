c5bf3972b9dc932f6678ad5cc8e094d701351ad7
Callum Young <callum.young@cgi.com>
2025-11-04 12:25:26 +0000

    Refactor inbox controller

--- server/routes/inbox/inbox.controller.js
+++ server/routes/inbox/inbox.controller.js
@@ -6,77 +6,69 @@
   const utils = require('../../lib/utils')
   const isCourtUser = require('../../components/auth/user-type').isCourtUser;
 
-  module.exports.index = function(app) {
-    return function(req, res) {
-      var highPriorityCount = 0
-        , standardPriorityCount  = 0
-        , successCB = function(response) {
-          var responseWasActioned;
+  module.exports.index = (app) => async (req, res) => {
+    // Clear session data
+    delete req.session.searchResponse;
+    delete req.session.formFields;
+    delete req.session.errors;
+    delete req.session.nav;
 
-          app.logger.info('Fetched and parsed list of todo responses: ', {
-            auth: req.session.authentication,
-          });
+    let highPriorityCount = 0;
+    let standardPriorityCount  = 0;
 
-          response.items = utils.sortResponseData(response.items, 'rawReceivedAt', false);
+    let courtLocCode;
 
-          // set alert details, counts
-          response.items.forEach(function(item) {
+    if (isCourtUser(req)){
+      courtLocCode = req.session.authentication.locCode;
+    }
 
-            item.alert = utils.getResponseAlert(item);
+    try {
+      const response = await responsesObj.query(req, 'todo', courtLocCode);
 
-            if ((item.isUrgent === true) || (item.slaOverdue === true)) {
-              highPriorityCount++;
-            } else {
-              standardPriorityCount++;
-            }
-          });
+      app.logger.info('Fetched and parsed list of todo responses: ', {
+        auth: req.session.authentication,
+      });
 
-          req.session.sourceUrl = req.path;
+      response.items = utils.sortResponseData(response.items, 'rawReceivedAt', false);
 
-          if (typeof req.session.responseWasActioned !== 'undefined') {
-            responseWasActioned = _.clone(req.session.responseWasActioned);
-            delete req.session.responseWasActioned;
-          }
-
-          res.locals.todoCount = response.counts.todo;
-          res.locals.workCount = response.counts.todo + response.counts.pending;
-
-          return res.render('inbox.njk', {
-            responses: response,
-            counts: {
-              highPriority: highPriorityCount,
-              standardPriority: standardPriorityCount,
-              total: response.items.length
-            },
-            responseWasActioned: responseWasActioned,
-          });
+      // set alert details, counts
+      response.items.forEach(function(item) {
+        item.alert = utils.getResponseAlert(item);
+        if ((item.isUrgent === true) || (item.slaOverdue === true)) {
+          highPriorityCount++;
+        } else {
+          standardPriorityCount++;
         }
+      });
 
-        , errorCB = function(err) {
-          app.logger.crit('Failed to fetch and parse list of to do responses: ', {
-            auth: req.session.authentication,
-            error: (typeof err.error !== 'undefined') ? err.error : err.toString(),
-          });
+      req.session.sourceUrl = req.path;
 
-          return res.render('index.njk');
-        }
-
-      // Clear session data
-      delete req.session.searchResponse;
-      delete req.session.formFields;
-      delete req.session.errors;
-      delete req.session.nav;
-
-      let courtLocCode;
-
-      if (isCourtUser(req)){
-        courtLocCode = req.session.authentication.locCode;
+      let responseWasActioned;
+      if (typeof req.session.responseWasActioned !== 'undefined') {
+        responseWasActioned = _.clone(req.session.responseWasActioned);
+        delete req.session.responseWasActioned;
       }
-      responsesObj
-        .query(req, 'todo', courtLocCode)
-        .then(successCB)
-        .catch(errorCB);
-      
-    };
+
+      res.locals.todoCount = response.counts.todo;
+      res.locals.workCount = response.counts.todo + response.counts.pending;
+
+      return res.render('inbox.njk', {
+        responses: response,
+        counts: {
+          highPriority: highPriorityCount,
+          standardPriority: standardPriorityCount,
+          total: response.items.length
+        },
+        responseWasActioned,
+      });
+    } catch (err) {
+      app.logger.crit('Failed to fetch and parse list of to do responses: ', {
+        auth: req.session.authentication,
+        error: (typeof err.error !== 'undefined') ? err.error : err.toString(),
+      });
+
+      return res.render('index.njk');
+    }  
   };
+  
 })();
