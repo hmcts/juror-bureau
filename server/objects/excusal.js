e2cd36d1a3c0370542ef1003d08a827b2891222e
Callum Young <callum.young@cgi.com>
2025-11-05 11:29:04 +0000

    Remove deprecated API endpoints

--- server/objects/excusal.js
+++ server/objects/excusal.js
@@ -3,24 +3,12 @@
   'use strict';
 
   const { DAO } = require('./dataAccessObject');
-  const urljoin = require('url-join')
   const { basicDataTransform } = require('../lib/utils');
 
   module.exports.object = new DAO('bureau/juror/excuse', {
     get: function() {
       return {
         uri: this.resource,
-        transform: basicDataTransform,
-      };
-    },
-    post: function(jurorNumber, version, excusalCode, reject = false) {
-      return {
-        uri: urljoin(this.resource, reject ? 'reject' : null, jurorNumber),
-        body: {
-          description: '',
-          excusalCode: excusalCode,
-          version: version,
-        },
         transform: basicDataTransform,
       };
     }
