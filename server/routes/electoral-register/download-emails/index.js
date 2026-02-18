095dbea5b14b4990401320aa31021dca57feaa61
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-02-18 09:31:28 +0000

    Add local authority email list download

---
+++ server/routes/electoral-register/download-emails/index.js
@@ -0,0 +1,18 @@
+(() => {
+  'use strict';
+
+  const controller = require('./download-emails.controller');
+  const auth = require('../../../components/auth');
+  const { isBureauUser } = require('../../../components/auth/user-type');
+
+  module.exports = function(app) {
+    app.get(
+      '/electoral-register/download-emails',
+      'electoral-register.download-emails.get',
+      auth.verify,
+      isBureauUser,
+      controller.getDownloadLaEmails(app)
+    );
+  };
+
+})();
