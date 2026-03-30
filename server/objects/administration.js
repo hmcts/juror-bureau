e02dd7fa09f0bcce872765a6321f7ffeb6ca8091
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-03-30 16:29:32 +0100

    Revert "Task/JS-820: Update the Administration management module to meet HMCTS API standards (#1139)"
    This reverts commit 7cd6d2dca3af0680e4658a5868e52d4aedf84d22.

--- server/objects/administration.js
+++ server/objects/administration.js
@@ -1,26 +1,21 @@
-(function () {
+;(function() {
   'use strict';
 
-  const _ = require('lodash');
   const { DAO } = require('./dataAccessObject');
   const urljoin = require('url-join');
-  const { extractDataAndHeadersFromResponse, replaceAllObjKeys } = require('../lib/mod-utils');
-  const { basicDataTransform2 } = require('../lib/utils');
+  const { extractDataAndHeadersFromResponse } = require('../lib/mod-utils');
 
   module.exports.systemCodesDAO = new DAO('moj/administration/codes', {
-    get: function (codeType) {
+    get: function(codeType) {
       return {
         uri: this.resource + `/${codeType}`,
-        transform: data => {
-          delete data['_headers'];
-          return replaceAllObjKeys(Object.values(data), _.camelCase);
-        },
+        transform: (data) => { delete data['_headers']; return Object.values(data) },
       };
-    },
-  });
+    }
+  })
 
   module.exports.deletePoolObject = new DAO('moj/manage-pool/delete', {
-    delete: function (poolNumber) {
+    delete: function(poolNumber) {
       return {
         uri: this.resource + `?poolNumber=${poolNumber}`,
       };
@@ -28,15 +23,15 @@
   });
 
   module.exports.expenseRatesAndLimitsDAO = new DAO('moj/administration/expenses/rates', {
-    get: function (etag = null) {
+    get: function(etag = null) {
       const headers = {};
 
       if (etag) {
         headers['If-None-Match'] = `${etag}`;
       }
 
-      return {
-        uri: this.resource,
+      return { 
+        uri: this.resource, 
         headers,
         transform: extractDataAndHeadersFromResponse(),
       };
@@ -44,154 +39,123 @@
   });
 
   module.exports.transportRatesDAO = new DAO('moj/court-location/{locCode}/rates', {
-    get: function (locCode, etag = null) {
+    get: function(locCode, etag = null) {
       const headers = {};
 
       if (etag) {
         headers['If-None-Match'] = `${etag}`;
       }
 
-      return {
+      return { 
         uri: this.resource.replace('{locCode}', locCode),
         headers,
         transform: extractDataAndHeadersFromResponse(),
       };
     },
-    put: function (locCode, body) {
-      return { uri: `moj/administration/courts/${locCode}/rates`, body };
-    },
+    put: function(locCode, body) {
+      return { uri: `moj/administration/courts/${locCode}/rates`, body};
+    }
   });
 
   module.exports.courtsDAO = new DAO('moj/administration/courts', {
-    get: function () {
-      return {
-        transform: data => {
-          delete data['_headers'];
-          return Object.values(data);
-        },
-      };
-    },
+    get: function() {
+      return { transform: (data) => { delete data['_headers']; return Object.values(data) } };
+    }
   });
 
   module.exports.courtroomsDAO = new DAO('moj/administration/court-rooms', {
-    get: function (locCode) {
-      return {
+    get: function(locCode) {
+      return { 
         uri: urljoin(this.resource, locCode),
-        transform: data => {
-          delete data['_headers'];
-          return Object.values(data);
-        },
+        transform: (data) => { delete data['_headers']; return Object.values(data) }
       };
     },
-    put: function (locCode, id, body) {
+    put: function(locCode, id, body) {
       return { uri: urljoin(this.resource, locCode, id), body };
     },
-    post: function (locCode, body) {
+    post: function(locCode, body) {
       return { uri: urljoin(this.resource, locCode), body };
     },
-  });
+  })
 
   module.exports.courtroomDetailsDAO = new DAO('moj/administration/court-rooms/{locCode}/{id}', {
-    get: function (locCode, id, etag = null) {
+    get: function(locCode, id, etag = null) {
       const headers = {};
 
       if (etag) {
         headers['If-None-Match'] = `${etag}`;
       }
 
-      return {
+      return { 
         uri: this.resource.replace('{locCode}', locCode).replace('{id}', id),
         headers,
         transform: extractDataAndHeadersFromResponse(),
       };
-    },
+    }
   });
 
   module.exports.judgesDAO = new DAO('moj/administration/judges', {
-    get: function (isActive) {
-      return {
+    get: function(isActive) {
+      return { 
         uri: this.resource + `?is_active=${isActive}`,
-        transform: data => {
-          delete data['_headers'];
-          return replaceAllObjKeys(Object.values(data), _.camelCase);
-        },
+        transform: (data) => { delete data['_headers']; return Object.values(data) } 
       };
     },
-    put: function (judgeId, body) {
-      return {
-        uri: this.resource + `/${judgeId}`,
-        body: replaceAllObjKeys(body, _.snakeCase),
-      };
+    put: function(judgeId, body) {
+      return { uri: this.resource + `/${judgeId}`, body };
     },
-    post: function (body) {
-      return {
-        uri: this.resource,
-        body: replaceAllObjKeys(body, _.snakeCase),
-      };
+    post: function(body) {
+      return { uri: this.resource, body };
     },
-    delete: function (judgeId) {
+    delete: function(judgeId) {
       return { uri: this.resource + `/${judgeId}` };
     },
   });
 
   module.exports.judgeDetailsDAO = new DAO('moj/administration/judges/{judgeId}', {
-    get: function (judgeId) {
-      return {
-        uri: this.resource.replace('{judgeId}', judgeId),
-        transform: basicDataTransform2,
-      };
-    },
+    get: function(judgeId) {
+      return { uri: this.resource.replace('{judgeId}', judgeId)};
+    }
   });
 
   module.exports.bankHolidaysDAO = new DAO('moj/administration/bank-holidays', {
-    get: function () {
-      return {
-        uri: this.resource,
-        transform: data => {
-          delete data['_headers'];
-          return replaceAllObjKeys(data, _.camelCase);
-        },
-      };
+    get: function() {
+      return { uri: this.resource, transform: (data) => { delete data['_headers']; return data} };
     },
-  });
+  })
 
   module.exports.nonSittingDayDAO = new DAO('moj/administration/non-sitting-days/{locCode}', {
-    get: function (locCode) {
-      return {
+    get: function(locCode) {
+      return { 
         uri: this.resource.replace('{locCode}', locCode),
-        transform: data => {
-          delete data['_headers'];
-          return replaceAllObjKeys(Object.values(data), _.camelCase);
-        },
+        transform: (data) => { delete data['_headers']; return Object.values(data) }
       };
     },
-    post: function (locCode, body) {
-      return {
-        uri: this.resource.replace('{locCode}', locCode),
-        body: replaceAllObjKeys(body, _.snakeCase),
-      };
+    post: function(locCode, body) {
+      return { uri: this.resource.replace('{locCode}', locCode), body };
     },
-    delete: function (locCode, date) {
+    delete: function(locCode, date) {
       return { uri: this.resource.replace('{locCode}', locCode) + `/${date}` };
     },
   });
 
   module.exports.courtDetailsDAO = new DAO('moj/administration/courts/{locCode}', {
-    get: function (locCode, etag = null) {
+    get: function(locCode, etag = null) {
       const headers = {};
 
       if (etag) {
         headers['If-None-Match'] = `${etag}`;
       }
 
-      return {
-        uri: this.resource.replace('{locCode}', locCode),
+      return { 
+        uri: this.resource.replace('{locCode}', locCode), 
         headers,
         transform: extractDataAndHeadersFromResponse(),
       };
     },
-    put: function (locCode, body) {
-      return { uri: this.resource.replace('{locCode}', locCode), body };
-    },
+    put: function(locCode, body) {
+      return { uri: this.resource.replace('{locCode}', locCode), body};
+    }
   });
+
 })();
