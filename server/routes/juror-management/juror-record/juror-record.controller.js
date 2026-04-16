6284d942dd73ec144b01934bf9a828d8e3726cd5
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-04-16 15:43:26 +0100

    Update juror-record.controller.js

--- server/routes/juror-management/juror-record/juror-record.controller.js
+++ server/routes/juror-management/juror-record/juror-record.controller.js
@@ -1284,6 +1284,7 @@
   }
 
   function canEnterSummons(req, commonDetails) {
+    console.log(commonDetails);
     const jurorStatus = resolveJurorStatus(commonDetails);
 
     if (commonDetails.owner !== req.session.authentication.owner) {
@@ -1294,7 +1295,7 @@
       return false;
     }
 
-    return !commonDetails.response_entered;
+    return !commonDetails.responseEntered;
   }
 
   const hasJurorNotes = (app) => async(req, res) => {
