382bf5c24828ee7c938d1c9fc228061513f5edb1
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-06-18 11:04:56 +0100

    Fix trial jury attendance confirmation

--- server/objects/jurors-on-trial.js
+++ server/objects/jurors-on-trial.js
@@ -15,10 +15,4 @@ module.exports.jurorsOnTrialDAO = new DAO('moj/juror-management/jurors-on-trial'
   },
 });
 
-module.exports.confirmAttendanceDAO = new DAO('moj/juror-management/confirm-jury-attendance', {
-  patch: function(body) {
-    return {
-      body: mapCamelToSnake(body),
-    };
-  }
-});
+module.exports.confirmAttendanceDAO = new DAO('moj/juror-management/confirm-jury-attendance');
