11dc252be4447a7a4823e8f1bb8725d251cf8d07
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-06-29 09:23:22 +0100

    Reinstate logging

---
+++ server/routes/documents/we-are-group/definitions.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    key: 'contactInformation',
+    path: 'we-are-group-contact-information',
+    routeName: 'documents.we-are-group-contact',
+    title: 'We are Group Contact Information',
+    code: 'CONTACT_INFORMATION',
+    messageContent: 'Thank you for contacting HM Courts & Tribunals Service (HMCTS) Jury Central Summoning Bureau.'
+      + '\n\nAs discussed today, if you would like support to complete your online form, you can contact our partner organisation We Are Group who can help. They do not provide legal advice.'
+      + '\n\nYou can contact them by:'
+      + '\nTelephone: 03300 160051'
+      + '\nOpening Hours: Monday - Friday 09:00 - 17:00'
+      + '\nEmail: support@wearegroup.com'
+      + '\nOr text "FORM" to 60777'
+      + '\nWebsite: https://www.wearegroup.com/digital_support'
+      + '\n\nPlease do not reply to this message as this number is not monitored.',
+  },
+  {
+    key: 'referralConfirmed',
+    path: 'we-are-group-referral-confirmed',
+    routeName: 'documents.we-are-group-referral-confirmed',
+    title: 'We Are Group Referral Confirmed',
+    code: 'REFERRAL_CONFIRMED',
+    messageContent: 'Thank you for contacting HM Courts and Tribunals Service (HMCTS) Jury Central Summoning Bureau.'
+    + '\n\nAs discussed today, we can confirm that we’ve made a referral to our partner organisation, We Are Group, to provide you with support for the online appeal form. They do not provide legal advice.'
+    + '\n\nYou will receive a call back from We Are Group within the next 2 working days. If you wish to contact them yourself, this can be done by:'
+    + '\n\Telephone: 03300 160051'
+    + '\nWe Are Group’s opening hours. Monday – Friday 09.00 – 17.00'
+    + '\nEmail: support@wearegroup.com'
+    + '\nOr text “FORM” to 60777'
+    + '\nWebsite: https://www.wearegroup.com/digital_support'
+    + '\n\nPlease do not reply to this message as this number is not monitored.'
+  },
+];
