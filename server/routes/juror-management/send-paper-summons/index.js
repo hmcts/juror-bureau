f4bf64d737ccb94f8d23558d07e1e7c79360e9f0
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-07-20 10:27:16 +0100

    Update feature flag on send paper summons routes

--- server/routes/juror-management/send-paper-summons/index.js
+++ server/routes/juror-management/send-paper-summons/index.js
@@ -4,19 +4,16 @@
   const auth = require('../../../components/auth');
   const controller = require('./send-paper-summons.controller');
   const jurorUpdateController = require('../update/juror-update.controller');
-  const config = require('../../../config/environment')();
 
   module.exports = function(app) {
-    if (config.featureFlags.digitalByDefault) {
-      app.get('/juror-management/juror/:jurorNumber/send-paper-summons',
-        'juror-management.send-paper-summons.get',
-        auth.verify,
-        controller.getSendPaperSummons(app));
-      app.post('/juror-management/juror/:jurorNumber/send-paper-summons',
-        'juror-management.send-paper-summons.post',
-        auth.verify,
-        controller.postSendPaperSummons(app));
-    }
+    app.get('/juror-management/juror/:jurorNumber/send-paper-summons',
+      'juror-management.send-paper-summons.get',
+      auth.verify,
+      controller.getSendPaperSummons(app));
+    app.post('/juror-management/juror/:jurorNumber/send-paper-summons',
+      'juror-management.send-paper-summons.post',
+      auth.verify,
+      controller.postSendPaperSummons(app));
   };
   
 })();
