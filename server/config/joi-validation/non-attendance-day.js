d497b44a9bf4c9c897e723998e8dd0348b5fbaca
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-08-05 09:52:57 +0100

    Add shared date and time picker validators

--- server/config/joi-validation/non-attendance-day.js
+++ server/config/joi-validation/non-attendance-day.js
@@ -1,7 +1,6 @@
 const Joi = require('joi');
-const moment = require('moment');
-const { parseDate } = require('./date-picker');
 const { validateJoiSchema } = require('./index');
+const { buildDatePickerSchema } = require('./date-validation');
 
 const requiredDateMessage = 'Enter a date for the non-attendance day';
 const invalidCharsMessage = 'Non-attendance date must only include numbers';
@@ -9,42 +8,13 @@ const invalidFormatMessage = 'Enter a non-attendance date in the correct format,
 const realDateMessage = 'Please enter a valid date for the non-attendance day';
 
 const schema = Joi.object({
-  nonAttendanceDay: Joi.any()
-    .optional()
-    .custom((value, helpers) => {
-      if (typeof value === 'undefined') {
-        return value;
-      }
-
-      if (value === '') {
-        return helpers.error('nonAttendanceDay.required');
-      }
-
-      if (/[^0-9\/]+/.test(value)) {
-        return helpers.error('nonAttendanceDay.invalidChars');
-      }
-
-      const dateInitial = parseDate(value);
-
-      if (!moment(dateInitial.dateAsDate).isValid()
-        || value.length > 10
-        || `${dateInitial.intYear}`.length === 2) {
-        return helpers.error('nonAttendanceDay.invalidFormat');
-      }
-
-      if (!dateInitial.isMonthAndDayValid) {
-        return helpers.error('nonAttendanceDay.realDate');
-      }
-
-      return value;
-    }, 'non-attendance day date validation')
-    .messages({
-      'any.required': requiredDateMessage,
-      'nonAttendanceDay.required': requiredDateMessage,
-      'nonAttendanceDay.invalidChars': invalidCharsMessage,
-      'nonAttendanceDay.invalidFormat': invalidFormatMessage,
-      'nonAttendanceDay.realDate': realDateMessage,
-    }),
+  nonAttendanceDay: buildDatePickerSchema({
+    field: 'nonAttendanceDay',
+    requiredMessage: requiredDateMessage,
+    invalidCharsMessage: invalidCharsMessage,
+    invalidFormatMessage: invalidFormatMessage,
+    realDateMessage: realDateMessage,
+  }),
 });
 
 module.exports = (body) => validateJoiSchema(schema, body);
--- server/config/joi-validation/unpaid-attendance.js
+++ server/config/joi-validation/unpaid-attendance.js
@@ -1,7 +1,6 @@
 const Joi = require('joi');
-const moment = require('moment');
-const { parseDate } = require('./date-picker');
 const { validateJoiSchema } = require('./index');
+const { buildDatePickerSchema } = require('./date-validation');
 
 const fromRequiredMessage = 'Enter a date to filter unpaid attendance from';
 const fromInvalidCharsMessage = '‘Date from’ can only include numbers and forward slashes';
@@ -11,41 +10,21 @@ const toInvalidCharsMessage = '‘Date to’ can only include numbers and forwar
 const toInvalidFormatMessage = 'Enter ‘date to‘  in the correct format, for example, 31/01/2023';
 const realDateMessage = 'Enter a real date';
 
-const buildDatePickerSchema = (requiredMessage, invalidCharsMessage, invalidFormatMessage) => Joi.string()
-  .empty('')
-  .required()
-  .pattern(/^[0-9/]+$/)
-  .custom((value, helpers) => {
-    const dateInitial = parseDate(value);
-
-    if (!moment(value, 'DD/MM/YYYY').isValid()) {
-      return helpers.error('datePicker.invalidFormat');
-    }
-
-    if (!dateInitial.isMonthAndDayValid) {
-      return helpers.error('datePicker.realDate');
-    }
-
-    return value;
-  }, 'unpaid attendance date validation')
-  .messages({
-    'any.required': requiredMessage,
-    'string.pattern.base': invalidCharsMessage,
-    'datePicker.invalidFormat': invalidFormatMessage,
-    'datePicker.realDate': realDateMessage,
-  });
-
 const schema = Joi.object({
-  unpaidAttendanceFromDate: buildDatePickerSchema(
-    fromRequiredMessage,
-    fromInvalidCharsMessage,
-    fromInvalidFormatMessage,
-  ),
-  unpaidAttendanceToDate: buildDatePickerSchema(
-    toRequiredMessage,
-    toInvalidCharsMessage,
-    toInvalidFormatMessage,
-  ),
+  unpaidAttendanceFromDate: buildDatePickerSchema({
+    field: 'unpaidAttendanceFromDate',
+    requiredMessage: fromRequiredMessage,
+    invalidCharsMessage: fromInvalidCharsMessage,
+    invalidFormatMessage: fromInvalidFormatMessage,
+    realDateMessage,
+  }),
+  unpaidAttendanceToDate: buildDatePickerSchema({
+    field: 'unpaidAttendanceToDate',
+    requiredMessage: toRequiredMessage,
+    invalidCharsMessage: toInvalidCharsMessage,
+    invalidFormatMessage: toInvalidFormatMessage,
+    realDateMessage,
+  }),
 });
 
 module.exports = (body) => validateJoiSchema(schema, {
