d16909fb5cafcfc65554ff554b91d21cefddefe0
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-08-05 09:52:57 +0100

    Add shared date and time picker validators

---
+++ server/config/joi-validation/add-attendance.js
@@ -0,0 +1,151 @@
+const Joi = require('joi');
+const { validateJoiSchema } = require('./index');
+const { buildDatePickerSchema } = require('./date-validation');
+const {
+  buildTimeFieldSchema,
+  buildTimeGroupSchema,
+  buildTimeComparisonValidator,
+  normaliseTimeBody,
+} = require('./time-validation');
+
+const attendanceDayMessages = {
+  required: 'Enter a date for the attendance day',
+  invalidFormat: 'Enter an attendance date in the correct format, for example, 31/01/2023',
+  invalidChars: 'Attendance date must only include numbers',
+  realDate: 'Please enter a valid date for the attendance day',
+  future: 'Attendance day cannot be in the future',
+};
+
+const timeMessages = {
+  checkIn: {
+    missingWholeTime: 'Enter a check in time',
+    missingHour: 'Enter an hour for check in time',
+    invalidChars: 'Check in time must only include numbers - you cannot enter letters or special characters',
+    invalidHour: 'Enter an hour between 0 and 12',
+    missingMinutes: 'Enter a minute for check in time',
+    invalidMinutes: 'Enter minutes between 0 and 59',
+    missingPeriod: 'Select whether check in time is am or pm',
+  },
+  checkOut: {
+    missingWholeTime: 'Enter a check out time',
+    missingHour: 'Enter an hour for check out time',
+    invalidChars: 'Check out time must only include numbers - you cannot enter letters or special characters',
+    invalidHour: 'Enter an hour between 0 and 12',
+    missingMinutes: 'Enter a minute for check out time',
+    invalidMinutes: 'Enter minutes between 0 and 59',
+    missingPeriod: 'Select whether check out time is am or pm',
+    beforeCheckIn: 'Check out time cannot be earlier than check in time',
+  },
+};
+
+const buildAttendanceDaySchema = () => buildDatePickerSchema({
+  field: 'attendanceDay',
+  requiredMessage: attendanceDayMessages.required,
+  invalidFormatMessage: attendanceDayMessages.invalidFormat,
+  invalidCharsMessage: attendanceDayMessages.invalidChars,
+  realDateMessage: attendanceDayMessages.realDate,
+  extraValidators: [
+    (value, dateInitial) => {
+      if (dateInitial.dateAsDate > new Date()) {
+        return 'datePicker.future';
+      }
+
+      return undefined;
+    },
+  ],
+  extraMessages: {
+    'datePicker.future': attendanceDayMessages.future,
+  },
+});
+
+const compareAttendanceTimes = buildTimeComparisonValidator({
+  earlierPrefix: 'checkInTime',
+  laterPrefix: 'checkOutTime',
+  message: timeMessages.checkOut.beforeCheckIn,
+});
+
+const schema = Joi.object({
+  attendanceDay: buildAttendanceDaySchema(),
+  checkInTime: buildTimeGroupSchema({
+    prefix: 'checkInTime',
+    message: timeMessages.checkIn.missingWholeTime,
+  }),
+  checkInTimeHour: buildTimeFieldSchema({
+    prefix: 'checkInTime',
+    part: 'Hour',
+    min: 1,
+    max: 12,
+    messages: {
+      missing: timeMessages.checkIn.missingHour,
+      invalidChars: timeMessages.checkIn.invalidChars,
+      invalidRange: timeMessages.checkIn.invalidHour,
+    },
+  }),
+  checkInTimeMinute: buildTimeFieldSchema({
+    prefix: 'checkInTime',
+    part: 'Minute',
+    min: 0,
+    max: 59,
+    messages: {
+      missing: timeMessages.checkIn.missingMinutes,
+      invalidChars: timeMessages.checkIn.invalidChars,
+      invalidRange: timeMessages.checkIn.invalidMinutes,
+    },
+  }),
+  checkInTimePeriod: buildTimeFieldSchema({
+    prefix: 'checkInTime',
+    part: 'Period',
+    messages: {
+      missing: timeMessages.checkIn.missingPeriod,
+    },
+  }),
+  checkOutTime: buildTimeGroupSchema({
+    prefix: 'checkOutTime',
+    message: timeMessages.checkOut.missingWholeTime,
+  }),
+  checkOutTimeHour: buildTimeFieldSchema({
+    prefix: 'checkOutTime',
+    part: 'Hour',
+    min: 1,
+    max: 12,
+    messages: {
+      missing: timeMessages.checkOut.missingHour,
+      invalidChars: timeMessages.checkOut.invalidChars,
+      invalidRange: timeMessages.checkOut.invalidHour,
+    },
+  }),
+  checkOutTimeMinute: buildTimeFieldSchema({
+    prefix: 'checkOutTime',
+    part: 'Minute',
+    min: 0,
+    max: 59,
+    messages: {
+      missing: timeMessages.checkOut.missingMinutes,
+      invalidChars: timeMessages.checkOut.invalidChars,
+      invalidRange: timeMessages.checkOut.invalidMinutes,
+    },
+  }),
+  checkOutTimePeriod: buildTimeFieldSchema({
+    prefix: 'checkOutTime',
+    part: 'Period',
+    messages: {
+      missing: timeMessages.checkOut.missingPeriod,
+    },
+  }),
+});
+
+module.exports = (body) => {
+  const normalisedBody = {
+    ...normaliseTimeBody(body, 'checkInTime'),
+    ...normaliseTimeBody(body, 'checkOutTime'),
+    ...body,
+  };
+
+  const validationResult = validateJoiSchema(schema, normalisedBody);
+
+  if (typeof validationResult !== 'undefined') {
+    return validationResult;
+  }
+
+  return compareAttendanceTimes(normalisedBody);
+};
--- server/config/joi-validation/approve-expenses.js
+++ server/config/joi-validation/approve-expenses.js
@@ -1,7 +1,8 @@
 const Joi = require('joi');
-const moment = require('moment');
-const { parseDate } = require('./date-picker');
 const { validateJoiSchema } = require('./index');
+const {
+  buildDatePickerSchema,
+} = require('./date-validation');
 
 const filterStartDateRequiredMessage = 'Enter a date you want to filter expenses from';
 const filterStartDateInvalidCharsMessage = '‘Date from’ can only include numbers and forward slashes';
@@ -11,39 +12,21 @@ const filterEndDateInvalidCharsMessage = '‘Date to’ can only include numbers
 const filterEndDateInvalidFormatMessage = 'Enter ‘date to‘  in the correct format, for example, 31/01/2023';
 const realDateMessage = 'Enter a real date';
 
-const buildDatePickerSchema = (requiredMessage, invalidCharsMessage, invalidFormatMessage) => Joi.string()
-  .empty('')
-  .required()
-  .pattern(/^[0-9/]+$/)
-  .custom((value, helpers) => {
-    if (!moment(value, 'DD/MM/YYYY').isValid()) {
-      return helpers.error('datePicker.invalidFormat');
-    }
-
-    if (!parseDate(value).isMonthAndDayValid) {
-      return helpers.error('datePicker.realDate');
-    }
-
-    return value;
-  }, 'approve expenses date validation')
-  .messages({
-    'any.required': requiredMessage,
-    'string.pattern.base': invalidCharsMessage,
-    'datePicker.invalidFormat': invalidFormatMessage,
-    'datePicker.realDate': realDateMessage,
-  });
-
 const schema = Joi.object({
-  approveExpensesFromDate: buildDatePickerSchema(
-    filterStartDateRequiredMessage,
-    filterStartDateInvalidCharsMessage,
-    filterStartDateInvalidFormatMessage,
-  ),
-  approveExpensesToDate: buildDatePickerSchema(
-    filterEndDateRequiredMessage,
-    filterEndDateInvalidCharsMessage,
-    filterEndDateInvalidFormatMessage,
-  ),
+  approveExpensesFromDate: buildDatePickerSchema({
+    field: 'approveExpensesFromDate',
+    requiredMessage: filterStartDateRequiredMessage,
+    invalidCharsMessage: filterStartDateInvalidCharsMessage,
+    invalidFormatMessage: filterStartDateInvalidFormatMessage,
+    realDateMessage,
+  }),
+  approveExpensesToDate: buildDatePickerSchema({
+    field: 'approveExpensesToDate',
+    requiredMessage: filterEndDateRequiredMessage,
+    invalidCharsMessage: filterEndDateInvalidCharsMessage,
+    invalidFormatMessage: filterEndDateInvalidFormatMessage,
+    realDateMessage,
+  }),
 });
 
 module.exports = (body) => validateJoiSchema(schema, {
