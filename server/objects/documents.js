d7de2d6a53dcc0f80539e14e93c6f7de0bf73ba7
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-09-03 09:39:13 +0100

    Task/JS-1081: Summons reminder letter validation (#1267)
    * Update yarn-audit-known-issues
    
    * Update summons reminders flow
    
    * Add validation page to summons reminder flow
    
    * Update summons-reminder-validation.njk
    
    * Update documents.js
    
    * Update yarn-audit-known-issues

--- server/objects/documents.js
+++ server/objects/documents.js
@@ -60,7 +60,7 @@
       const dao = new DAO('moj/letter/reissue-letter', {
         post: function(body) {
           return {
-            body: _.cloneDeep(body),
+            body: mapCamelToSnake(_.cloneDeep(body)),
             transform: (data) => {
               delete data['_headers'];
               return mapSnakeToCamel(data);
@@ -93,7 +93,7 @@
       const dao = new DAO('moj/letter/delete-pending-letter', {
         delete: function(body) {
           return {
-            body: _.cloneDeep(body),
+            body: mapCamelToSnake(_.cloneDeep(body)),
           };
         },
       });
@@ -167,6 +167,18 @@
       return {
         uri: urljoin(this.resource, jurorNumber),
         transform: basicDataTransform,
+      };
+    },
+  });
+
+  module.exports.getSummonsReminderValidationDAO = new DAO('moj/letter/validate-reissue-letter', {
+    post: function(body) {
+      return {
+        body: mapCamelToSnake(_.cloneDeep(body)),
+        transform: (data) => {
+          delete data['_headers'];
+          return mapSnakeToCamel(data);
+        },
       };
     },
   });
