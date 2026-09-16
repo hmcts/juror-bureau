65e28596410915d4f996f057f54d029a6ed0e600
Callum Young <148960132+callum-r-young@users.noreply.github.com>
2026-09-16 09:16:29 +0100

    Add paper pack document

--- server/lib/mod-utils.js
+++ server/lib/mod-utils.js
@@ -1272,6 +1272,8 @@
     switch (pageType){
     case 'initial-summons':
       return 'Initial summons';
+    case 'paper-packs-sent':
+      return 'Paper packs sent';
     case 'summons-reminders':
       return 'Summons reminders';
     case 'further-information':
@@ -1302,6 +1304,7 @@
 
   module.exports.LetterType = {
     'initial-summons': 'SUMMONS',
+    'paper-packs-sent': 'RESPONSE',
     'summons-reminders': 'SUMMONED_REMINDER',
     'further-information': 'INFORMATION',
     'confirmation': 'CONFIRMATION',
